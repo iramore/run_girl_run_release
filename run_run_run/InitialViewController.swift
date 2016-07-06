44febe600979a57e20ee8dce204ed54250a3c151
infuntis <infuntis@MacBook-Pro.local>
2016-07-06 14:37:23 +0300

    transition works

--- run_run_run/InitialViewController.swift
+++ run_run_run/InitialViewController.swift
@@ -78,6 +78,35 @@ class InitialViewController: UIViewController {
         transition.panThreshold = 0.3
         transition.transformType = .TranslateMid
         
+        
+        let image1  = UIImage(named: "plan-but") as UIImage?
+        
+        let frame1 = CGRectMake(self.view.bounds.width/2 - (image1?.size.width)!,self.view.bounds.height*2/3 - (image1?.size.height)!, (image1?.size.width)! , (image1?.size.height)!)
+        
+        
+        let planButton = MenuButton(path: firstButtonBezier(), frame: frame1, image: "plan-but")
+        planButton.addTarget(self, action: #selector(didPressTriangle) , forControlEvents: UIControlEvents.TouchUpInside)
+        
+        let image2  = UIImage(named: "r-b") as UIImage?
+        
+        let frame2 = CGRectMake(self.view.bounds.width/2 ,self.view.bounds.height*2/3 - (image2?.size.height)!, (image2?.size.width)! , (image2?.size.height)!)
+        
+        
+        let runButton = MenuButton(path: secondButtonBezier(), frame: frame2, image: "r-b")
+        runButton.addTarget(self, action: #selector(didPressSquare), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        let image3  = UIImage(named: "track-but") as UIImage?
+        
+        let frame3 = CGRectMake(self.view.bounds.width/2 - (image3?.size.width)!/2 ,self.view.bounds.height*2/3 - (image3?.size.height)!/2, (image3?.size.width)! , (image3?.size.height)!)
+        
+        
+        let trackButton = MenuButton(path: thirdButtonBezier(), frame: frame3, image: "track-but")
+        trackButton.addTarget(self, action: #selector(didPressPentagon), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(planButton)
+        self.view.addSubview(runButton)
+        self.view.addSubview(trackButton)
+        
     }
     
     
@@ -91,6 +120,8 @@ class InitialViewController: UIViewController {
         transition.edge = .Bottom
         transition.startingPoint = sender.center
         performSegueWithIdentifier("option", sender: self)
+//        let modalViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("calendarControl") as! CalViewController
+//        presentViewController(modalViewController, animated: true, completion: nil)
     }
     
     @IBAction func aboutBtnTouched(sender: AnyObject) {
@@ -110,5 +141,105 @@ class InitialViewController: UIViewController {
             vc.transition = transition
         }
     }
+    
+    func buttonAction(sender: UIButton!) {
+        print("Button tapped")
+    }
+    
+    
+    func didPressTriangle(sender: AnyObject?) {
+        transition.edge = .Left
+        transition.startingPoint = sender!.center
+        performSegueWithIdentifier("plan", sender: self)
+    }
+    
+    func didPressSquare(sender: AnyObject?) {
+        transition.edge = .Right
+        transition.startingPoint = sender!.center
+        performSegueWithIdentifier("run", sender: self)
+    }
+    
+    func didPressPentagon(sender: AnyObject?) {
+        transition.edge = .Bottom
+        transition.startingPoint = sender!.center
+        performSegueWithIdentifier("option", sender: self)
+//        transition.edge = .Bottom
+//        transition.startingPoint = sender!.center
+//        let modalViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("calendarControl") as! CalViewController
+//        modalViewController.transition = transition
+//        presentViewController(modalViewController, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func firstButtonBezier() -> UIBezierPath {
+        let ovalPath = UIBezierPath(ovalInRect: CGRect(x: 71, y: 38, width: 45, height: 41))
+        UIColor.grayColor().setFill()
+        ovalPath.fill()
+        //// Bezier Drawing
+        let bezierPath = UIBezierPath()
+        bezierPath.moveToPoint(CGPoint(x: 126.84, y: 126.82))
+        bezierPath.addLineToPoint(CGPoint(x: 16.76, y: 189.82))
+        bezierPath.addLineToPoint(CGPoint(x: 17.04, y: 190.3))
+        bezierPath.addCurveToPoint(CGPoint(x: 63.46, y: 17.06), controlPoint1: CGPoint(x: -17.98, y: 129.65), controlPoint2: CGPoint(x: 2.8, y: 52.08))
+        bezierPath.addLineToPoint(CGPoint(x: 63.05, y: 17.31))
+        bezierPath.addCurveToPoint(CGPoint(x: 125.5, y: -0), controlPoint1: CGPoint(x: 81.91, y: 5.98), controlPoint2: CGPoint(x: 103.5, y: -0))
+        bezierPath.addLineToPoint(CGPoint(x: 126.84, y: 126.82))
+        bezierPath.closePath()
+        bezierPath.miterLimit = 4
+        bezierPath.fill()
+        
+        return bezierPath
+    }
+    
+    
+    func secondButtonBezier() -> UIBezierPath {
+        
+        let fillColor = UIColor(red: 0.939, green: 0.847, blue: 0.502, alpha: 1.000)
+        
+        //// Bezier Drawing
+        let bezierPath = UIBezierPath()
+        bezierPath.moveToPoint(CGPoint(x: 0, y: 126.82))
+        bezierPath.addLineToPoint(CGPoint(x: 0, y: -0.02))
+        bezierPath.addLineToPoint(CGPoint(x: 0, y: -0.02))
+        bezierPath.addCurveToPoint(CGPoint(x: 126.82, y: 126.8), controlPoint1: CGPoint(x: 70.04, y: -0.02), controlPoint2: CGPoint(x: 126.82, y: 56.76))
+        bezierPath.addLineToPoint(CGPoint(x: 126.81, y: 126.18))
+        bezierPath.addCurveToPoint(CGPoint(x: 110.58, y: 188.92), controlPoint1: CGPoint(x: 127.19, y: 148.18), controlPoint2: CGPoint(x: 121.59, y: 169.87))
+        bezierPath.addLineToPoint(CGPoint(x: 0, y: 126.82))
+        bezierPath.closePath()
+        bezierPath.miterLimit = 4;
+        
+        fillColor.setFill()
+        bezierPath.fill()
+        
+        return bezierPath
+    }
+    
+    
+    func thirdButtonBezier() -> UIBezierPath {
+        
+        
+        let fillColor = UIColor(red: 0.708, green: 0.826, blue: 0.887, alpha: 1.000)
+        
+        //// Bezier Drawing
+        let bezierPath = UIBezierPath()
+        bezierPath.moveToPoint(CGPoint(x: 108.67, y: 0))
+        bezierPath.addLineToPoint(CGPoint(x: 218.67, y: 63.14))
+        bezierPath.addLineToPoint(CGPoint(x: 218.52, y: 63.41))
+        bezierPath.addCurveToPoint(CGPoint(x: 45.28, y: 109.83), controlPoint1: CGPoint(x: 183.49, y: 124.07), controlPoint2: CGPoint(x: 105.93, y: 144.85))
+        bezierPath.addLineToPoint(CGPoint(x: 45.83, y: 110.14))
+        bezierPath.addCurveToPoint(CGPoint(x: -0.39, y: 64.72), controlPoint1: CGPoint(x: 26.59, y: 99.48), controlPoint2: CGPoint(x: 10.61, y: 83.77))
+        bezierPath.addLineToPoint(CGPoint(x: 108.67, y: 0))
+        bezierPath.closePath()
+        bezierPath.miterLimit = 4;
+        
+        fillColor.setFill()
+        bezierPath.fill()
+        
+        return bezierPath
+    }
+
     
 }
---
+++ run_run_run/MenuButton.swift
@@ -0,0 +1,50 @@
+
+
+import UIKit
+
+class MenuButton: UIButton {
+    
+   
+    var bezierPath: UIBezierPath = UIBezierPath()
+    
+    var action: ((AnyObject?) -> ())? = nil
+    
+    init(path: UIBezierPath,  frame: CGRect, image: String) {
+        super.init(frame: frame)
+        
+        
+        let image  = UIImage(named: image) as UIImage?
+        self.setImage(image, forState: .Normal)
+        //self.backgroundColor = UIColor.blackColor()
+        bezierPath = path;
+
+        let mask = CAShapeLayer()
+        mask.frame = self.bounds
+        mask.path = bezierPath.CGPath
+        self.layer.mask = mask
+        self.frame = frame
+        self.addTarget(self, action: "buttonAction:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    func buttonAction(sender: UIButton) {
+        action?(sender)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func hitTest(point: CGPoint, withEvent event: UIEvent?) -> UIView? {
+        
+        //return super.hitTest(point, withEvent: event)
+        if bezierPath.containsPoint(point) {
+            return self
+        }
+        return nil
+    }
+}
